74a5a50d4b24975e3893158b81136cbe005067e1
ZaidanFathan <goldhair243@gmail.com>
2023-06-21 17:41:58 +0700

    Menambahkan middleware pada route frontend

--- routes/web.php
+++ routes/web.php
@@ -19,19 +19,23 @@ Route::get('/', function () {
 
 
 // Route halaman frontend
-Route::prefix('produk')->group(function() {
-    Route::get('/', [App\Http\Controllers\Frontend\ProdukController::class, 'index']);
-    Route::get('cart', [App\Http\Controllers\Frontend\CartController::class, 'index']);
-    Route::get('cart/insert/{id}', [App\Http\Controllers\Frontend\CartController::class, 'insert']);
-    Route::get('cart/delete/{id}', [App\Http\Controllers\Frontend\CartController::class, 'delete']);
+Route::group(['middleware' => ['auth', 'role:user']], function() {
+    Route::prefix('produk')->group(function() {
+        Route::get('/', [App\Http\Controllers\Frontend\ProdukController::class, 'index']);
+        Route::get('cart', [App\Http\Controllers\Frontend\CartController::class, 'index']);
+        Route::get('cart/insert/{id}', [App\Http\Controllers\Frontend\CartController::class, 'insert']);
+        Route::get('cart/delete/{id}', [App\Http\Controllers\Frontend\CartController::class, 'delete']);
+    });
+
+    Route::prefix('pesanan')->group(function() {
+        Route::get('/', [App\Http\Controllers\Frontend\PesananController::class, 'index']);
+        Route::get('/form', [App\Http\Controllers\Frontend\PesananController::class, 'get']);
+        Route::post('/proses', [App\Http\Controllers\Frontend\PesananController::class, 'create']);
+    });
 });
 
 
-Route::prefix('pesanan')->group(function() {
-    Route::get('/', [App\Http\Controllers\Frontend\PesananController::class, 'index']);
-    Route::get('/form', [App\Http\Controllers\Frontend\PesananController::class, 'get']);
-    Route::post('/proses', [App\Http\Controllers\Frontend\PesananController::class, 'create']);
-});
+
 Auth::routes();
 Route::group(['middleware' => ['auth', 'role:admin']], function () {
     Route::prefix('admin')->group(function () {
