d1b72e1ad7c2cfb39dbee3ad3f4d6ef969ba4ca1
aexto9 <mushab0911@gmail.com>
2023-06-28 14:02:34 +0700

    user - mushab

--- routes/web.php
+++ routes/web.php
@@ -54,10 +54,11 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-        Route::get('/user', [App\Http\Controllers\Backend\UserController::class, 'index']);
-        Route::get('/user/create', [App\Http\Controllers\Backend\UserController::class, 'create']);
-        Route::get('/user/edit/{id}', [App\Http\Controllers\Backend\UserController::class, 'edit']);
-        Route::get('/user/show/{id}', [App\Http\Controllers\Backend\UserController::class, 'show']);
-        Route::post('/user/store', [App\Http\Controllers\Backend\UserController::class, 'store']);
-        Route::delete('/user/destroy/{id}', [App\Http\Controllers\Backend\UserController::class, 'destroy']);
-        Route::put('/user/update/{id}', [App\Http\Controllers\Backend\UserController::class, 'update']);
+//belum dimasukin ke khusus admin ya bang
+Route::get('/user', [App\Http\Controllers\Backend\UserController::class, 'index']);
+Route::get('/user/create', [App\Http\Controllers\Backend\UserController::class, 'create']);
+Route::get('/user/edit/{id}', [App\Http\Controllers\Backend\UserController::class, 'edit']);
+Route::get('/user/show/{id}', [App\Http\Controllers\Backend\UserController::class, 'show']);
+Route::post('/user/store', [App\Http\Controllers\Backend\UserController::class, 'store']);
+Route::delete('/user/destroy/{id}', [App\Http\Controllers\Backend\UserController::class, 'destroy']);
+Route::put('/user/update/{id}', [App\Http\Controllers\Backend\UserController::class, 'update']);
