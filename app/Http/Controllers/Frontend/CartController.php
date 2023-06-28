f9ed3a374413cc48d2e5747112627dfa56e558de
ZaidanFathan <goldhair243@gmail.com>
2023-06-28 17:41:44 +0700

    Edit Controller Produk dan Cart

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -5,17 +5,18 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use DB;
-
+use Auth;
 class CartController extends Controller
 {
     public function index(){
-        $total = DB::table('cart')->get()->count();
+        $total = DB::table('cart')->where('users_id', Auth::user()->id)->get()->count();
         session()->put('CartTotal', $total);
 
         $cart = DB::table('cart')
         ->join('produk', 'produk.id', '=', 'cart.produk_id')
         ->join('kategori_produk', 'kategori_produk.id', '=', 'produk.kategori_produk_id')
         ->select('produk.*', 'kategori_produk.nama AS Kategori', 'cart.total AS Total_Pesanan')
+        ->where('users_id', Auth::user()->id)
         ->get();
 
         return view('frontend/Cart/index', compact('cart'));
@@ -31,7 +32,7 @@ class CartController extends Controller
             DB::table('cart')->insert([
                 'cart.produk_id' => $id,
                 'total' => 1,
-                'users_id' => 2
+                'users_id' => Auth::user()->id
             ]);
         }
         return redirect('/produk/cart');
